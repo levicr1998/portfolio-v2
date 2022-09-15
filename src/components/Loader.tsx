4c5844fa4d32768b78178c0637eb2d1de9c1154b
Levi Crietee <levi.crietee@isaac.nl>
2022-09-15 20:05:46 +0200

    Added icons and components

---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+
+
+const Loader = () => <div>
+    <div className="bg-amber-500 w-full h-screen">
+        <div className="flex items-center justify-center h-full">
+        <img className="w-20" src="../assets/img/icons/spinning-circles.svg" />
+        </div>
+    </div>
+</div>
+
+export default Loader
